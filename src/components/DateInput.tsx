7affb106928ae84307091905fe89556fca537c08
BrankoMuruaga <muruagabranko253@gmail.com>
2025-05-10 10:20:40 -0300

    feat: agregar componentes DateInput, SelectInput y TextInput

---
+++ src/components/DateInput.tsx
@@ -0,0 +1,21 @@
+// import Calendar from "../icons/Calendar";
+
+function DateInput({
+  onChange,
+}: {
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <div className="w-full flex gap-4 relative border-2 border-wild-sand-600 rounded-lg p-2 justify-between">
+      <input
+        className="form-date__input text-wild-sand-600 "
+        type="date"
+        id="input-date"
+        onChange={onChange}
+      />
+      {/* <Calendar /> */}
+    </div>
+  );
+}
+
+export default DateInput;
