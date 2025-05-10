6e4bfbb800bbfac7adf808fab836a0a618e7a688
BrankoMuruaga <muruagabranko253@gmail.com>
2025-05-10 10:12:29 -0300

    feat: refactor Table component to use TableColumn and enhance TableRow with action handlers

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,51 @@
 import React from "react";
-import TableHeader from "./TableHeader";
+import TableColumn from "./TableColumn";
 import TableRow from "./TableRow";
+import { log } from "console";
 
-function Table({ data }: { data: any }) {
-  const headers = Object.keys(data[0]); // ["id", "name", "age", "email"]
-  const rows = data.map((item: any) => Object.values(item)); // [[1, "John Doe", 30, ""], [1, "John Doe", 30, ""]]
+interface TableProps {
+  data: any[];
+  viewButton?: boolean;
+  editButton?: boolean;
+  deleteButton?: boolean;
+  onView?: (row: any) => void;
+  onEdit?: (row: any) => void;
+  onDelete?: (id: string | number) => void;
+}
+
+function Table({
+  data,
+  viewButton,
+  editButton,
+  deleteButton,
+  onView,
+  onEdit,
+  onDelete,
+}: TableProps) {
+  const baseColumns = Object.keys(data[0]);
+  const hasActions = viewButton || editButton || deleteButton;
+  const columns = hasActions ? [...baseColumns, "Acciones"] : baseColumns;
 
   return (
-    <table className="overflow-hidden w-full bg-wild-sand-100 shadow-md rounded-2xl  border-gray-300">
-      <TableHeader data={headers} />
+    <table className="overflow-hidden w-full bg-wild-sand-100 shadow-md rounded-2xl border-gray-300">
+      {/* columns */}
+      <thead className="bg-gray-300 rounded-2xl">
+        <TableColumn columns={columns} />
+      </thead>
+
+      {/* Rows */}
       <tbody className="p-3">
-        {rows.map((row: unknown, index: number) => (
-          <TableRow key={index} deleteButton editButton row={row} />
+        {data.map((row, index) => (
+          <TableRow
+            key={index}
+            row={row}
+            viewButton={viewButton}
+            editButton={editButton}
+            deleteButton={deleteButton}
+            onView={onView}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
         ))}
       </tbody>
     </table>
