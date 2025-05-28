b8b30b22c2c51210ff010141769bc0c23bbb4fcf
DubuisPatricio <69608909+DubuisPatricio@users.noreply.github.com>
2025-05-28 17:07:50 -0300

    ABM transportista y adicional actualizado y conectado con tarifa

--- src/hooks/useTarifaFormData.ts
+++ src/hooks/useTarifaFormData.ts
@@ -1,29 +1,15 @@
 import { useCargas } from "./useCargas";
 import { useVehiculos } from "./useVehiculos";
 import { useZonas } from "./useZonas";
+import { useAdicional } from "./useAdicional";
+import { useTransportista } from "./useTransportista";
 
 export const useTarifaFormData = () => {
   const { data: cargas } = useCargas();
   const { data: vehiculos } = useVehiculos();
   const { data: zonas } = useZonas();
-
-  // Mock de adicionales siguiendo el modelo de la base de datos
-  const adicionales = [
-    {
-      id: 1,
-      tipo: "Otro",
-      costo_default: 2000,
-    },
-    {
-      id: 2,
-      tipo: "Peon",
-      costo_default: 100,
-    },
-  ];
-
-  // Mock de transportistas siguiendo el modelo de la base de datos
-  const transportistas = [{ id: 1, nombre: "Transporte" }];
-
+  const { data: adicionales } = useAdicional();
+  const { data: transportistas } = useTransportista();
   return {
     cargas,
     vehiculos,
