1a289d553f4b9cdd3e3e8df6a890fa1bc3c04df4
BrankoMuruaga <muruagabranko253@gmail.com>
2025-06-06 15:53:37 -0300

    Ajustar estilos de etiqueta en el componente TextInput para mejorar la legibilidad

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -20,7 +20,7 @@ function TextInput({
       <div
         className={`relative w-full flex justify-center items-center rounded-lg p-2 border-2 border-wild-sand-600 focus:border-wild-sand-600 active:border-wild-sand-600 py-0 ${className}`}
       >
-        <p className="text-wild-sand-600 text-sm z-10 px-1 left-4 -top-3 absolute bg-gradient-to-t from-gray-chateau-50 from-70% to-transparent ">
+        <p className="text-gray-400 text-xs z-10 px-1 left-3 -top-2 absolute bg-gradient-to-t from-gray-chateau-50 from-70% to-transparent ">
           {label}
         </p>
         <input
